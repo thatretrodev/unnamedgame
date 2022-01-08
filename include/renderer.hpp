35bfeddd46a51f5e140fff3f2b35f120708b2a8e
thatretrodev <thatretrodev@outlook.com>
2022-01-08 15:58:07 -0500

    Remove all code

--- include/renderer.hpp
+++
@@ -1,9 +0,0 @@
-#include "model.hpp"
-
-namespace Renderer {
-	void Init(char* title, int windowWidth, int windowHeight);
-	void PreRender(bool* isRunning);
-	void RenderModel(Model model, float size);
-	void PostRender();
-	//void Unload();
-}
