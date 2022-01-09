9292a8d6a02206b0cceb063e24d53dbd2e048f3d
thatretrodev <thatretrodev@outlook.com>
2022-01-09 14:19:01 -0500

    Update the actual game scene

--- src/scenes/gamescene.cpp
+++ src/scenes/gamescene.cpp
@@ -4,14 +4,23 @@
 #include "gamescene.hpp"
 
 void GameScene::Init() {
-
+	this->camera = { 0 };
+	this->camera.position = (Vector3){ 0.0f, 10.0f, 10.0f };
+	this->camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
+	this->camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+	this->camera.fovy = 45.0f;
+	this->camera.projection = CAMERA_PERSPECTIVE;
 }
 
 void GameScene::Render() {
-	ClearBackground(DARKGRAY);
+	ClearBackground(WHITE);
 	
-	char* text = "Hello, World!";
-	int textWidth = MeasureText(text, 20);
+	BeginMode3D(this->camera);
 
-	DrawText(text, (GetScreenWidth() / 2) - (textWidth / 2), GetScreenHeight() / 2, 20, WHITE);
+	DrawCube(this->cubePosition, 2.0f, 2.0f, 2.0f, RED);
+	DrawCubeWires(this->cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
+
+	DrawGrid(10, 1.0f);
+
+	EndMode3D();
 }
--- src/scenes/gamescene.hpp
+++ src/scenes/gamescene.hpp
@@ -1,4 +1,7 @@
 class GameScene {
+	Camera3D camera;
+	Vector3 cubePosition;
+	
 	public:
 		void Init();
 		void Render();
